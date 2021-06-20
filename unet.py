94cfabc41b6203a2c6b98d01bdde25d659114933
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2021-06-20 13:34:01 -0500

    working class implementation

--- unet.py
+++ unet.py
@@ -10,16 +10,16 @@ from tensorflow.keras import Model, Input, layers, initializers
 import numpy as np
 
 class Unet(Model):
-    def __init__(self, input_shape, K, number_of_filters=64, levels=4, activation='relu', kernel_initializer=initializers.HeNormal):
+    def __init__(self, input_shape, K, starting_number_of_filters=64, levels=4, activation='relu', kernel_initializer=initializers.HeNormal):
         self.K = K
         input = Input(shape = input_shape)
         
-        
+        number_of_filters = starting_number_of_filters
         counterparts = []
         previous_down = input
         
         for i in range(levels):
-            down, counterpart = contracting_block(previous_down, number_of_filters)
+            down, counterpart = self._contracting_block(previous_down, number_of_filters, activation=activation, kernel_initializer=kernel_initializer)
             counterparts.insert(0, counterpart)
 
             number_of_filters *= 2
@@ -28,26 +28,26 @@ class Unet(Model):
         previous_smashed_together = previous_down
 
         for i in range(levels):
-            smashed_together = expanding_block(previous_smashed_together, counterparts[i], number_of_filters)
+            smashed_together = self._expanding_block(previous_smashed_together, counterparts[i], number_of_filters, activation=activation, kernel_initializer=kernel_initializer)
 
             number_of_filters /= 2
             previous_smashed_together = smashed_together
 
         final_concat = previous_smashed_together
-        conv = layers.Conv2D(number_of_filters, 3, activation='relu', kernel_initializer=initializers.HeNormal)(final_concat)
-        conv = layers.Conv2D(number_of_filters, 3, activation='relu', kernel_initializer=initializers.HeNormal)(conv)
-        output = layers.Conv2D(K, 1, activation='softmax', kernel_initializer=initializers.HeNormal, name='maps')(conv)
+        conv = layers.Conv2D(number_of_filters, 3, activation=activation, kernel_initializer=kernel_initializer)(final_concat)
+        conv = layers.Conv2D(number_of_filters, 3, activation=activation, kernel_initializer=kernel_initializer)(conv)
+        output = layers.Conv2D(K, 1, activation='softmax', kernel_initializer=kernel_initializer, name='maps')(conv)
 
-        super(input, output)
+        super().__init__(input, output)
 
-    def contracting_block(previous_down, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
+    def _contracting_block(self, previous_down, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
         right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(previous_down)
         right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
         down = layers.MaxPool2D()(right_convolution)
 
         return down, right_convolution
 
-    def _expanding_block(previous_smashed_together, contracting_counterpart, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
+    def _expanding_block(self, previous_smashed_together, contracting_counterpart, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
         right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(previous_smashed_together)
         right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
         up = layers.Conv2DTranspose(number_of_filters/2, 2, strides=2, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
@@ -57,10 +57,11 @@ class Unet(Model):
 
 
 
-model.summary()
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy','val_accuracy'])
+if __name__ == "__main__":
+    
+    model = Unet(input_shape=(572, 572, 1), K=3)
+    model.summary()
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy','val_accuracy'])
 
 
-#model.predict(np.empty((3,572,572,1)))
-
-shape=(572, 572, 1)
+    #model.predict(np.empty((3,572,572,1)))
