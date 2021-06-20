54e7078c45da88cc8e27a962006ab794c3140aa3
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2021-06-20 13:09:11 -0500

    starting class based implementation

--- unet.py
+++ unet.py
@@ -9,60 +9,58 @@ import tensorflow as tf
 from tensorflow.keras import Model, Input, layers, initializers
 import numpy as np
 
-def contracting_block(previous_down, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
-    right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(previous_down)
-    right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
-    down = layers.MaxPool2D()(right_convolution)
+class Unet(Model):
+    def __init__(self, input_shape, K, number_of_filters=64, levels=4, activation='relu', kernel_initializer=initializers.HeNormal):
+        self.K = K
+        input = Input(shape = input_shape)
+        
+        
+        counterparts = []
+        previous_down = input
+        
+        for i in range(levels):
+            down, counterpart = contracting_block(previous_down, number_of_filters)
+            counterparts.insert(0, counterpart)
 
-    return down, right_convolution
+            number_of_filters *= 2
+            previous_down = down
 
-def expanding_block(previous_smashed_together, contracting_counterpart, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
-    right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(previous_smashed_together)
-    right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
-    up = layers.Conv2DTranspose(number_of_filters/2, 2, strides=2, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
-    smashed_together = layers.Concatenate()([tf.image.resize(contracting_counterpart, up.shape[1:3]), up])
+        previous_smashed_together = previous_down
 
-    return smashed_together
+        for i in range(levels):
+            smashed_together = expanding_block(previous_smashed_together, counterparts[i], number_of_filters)
 
-K = 3
+            number_of_filters /= 2
+            previous_smashed_together = smashed_together
 
+        final_concat = previous_smashed_together
+        conv = layers.Conv2D(number_of_filters, 3, activation='relu', kernel_initializer=initializers.HeNormal)(final_concat)
+        conv = layers.Conv2D(number_of_filters, 3, activation='relu', kernel_initializer=initializers.HeNormal)(conv)
+        output = layers.Conv2D(K, 1, activation='softmax', kernel_initializer=initializers.HeNormal, name='maps')(conv)
 
-input = Input(shape=(572, 572, 1))
+        super(input, output)
 
-number_of_filters = 64
-counterparts = []
-previous_down = input
-i = 0
-while i < 4:
-    down, counterpart = contracting_block(previous_down, number_of_filters)
-    counterparts.insert(0, counterpart)
+    def contracting_block(previous_down, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
+        right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(previous_down)
+        right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
+        down = layers.MaxPool2D()(right_convolution)
 
-    number_of_filters *= 2
-    previous_down = down
-    i += 1
+        return down, right_convolution
 
-previous_smashed_together = previous_down
-i = 0
-while i < 4:
-    smashed_together = expanding_block(previous_smashed_together, counterparts[i], number_of_filters)
+    def _expanding_block(previous_smashed_together, contracting_counterpart, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal):
+        right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(previous_smashed_together)
+        right_convolution = layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
+        up = layers.Conv2DTranspose(number_of_filters/2, 2, strides=2, activation=activation, kernel_initializer=kernel_initializer)(right_convolution)
+        smashed_together = layers.Concatenate()([tf.image.resize(contracting_counterpart, up.shape[1:3]), up])
 
-    number_of_filters /= 2
-    previous_smashed_together = smashed_together
-    i += 1
-
-
-final_concat = previous_smashed_together
-
-conv = layers.Conv2D(number_of_filters, 3, activation='relu', kernel_initializer=initializers.HeNormal)(final_concat)
-conv = layers.Conv2D(number_of_filters, 3, activation='relu', kernel_initializer=initializers.HeNormal)(conv)
-
-output = layers.Conv2D(K, 1, activation='softmax', kernel_initializer=initializers.HeNormal, name='maps')(conv)
+        return smashed_together
 
 
 
-model = Model(input, output)
 model.summary()
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy','val_accuracy'])
 
 
 #model.predict(np.empty((3,572,572,1)))
+
+shape=(572, 572, 1)
