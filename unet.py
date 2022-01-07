2bb75a28b78d9ecacd65f857d34adcf9e1ddb4a7
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2022-01-07 14:26:50 -0600

    further abstracting the implementation

--- unet.py
+++ unet.py
@@ -8,9 +8,45 @@
 import tensorflow as tf
 from tensorflow.keras import Model, Input, layers, initializers
 import numpy as np
+import tensorflow.keras as keras
+
+class ContractingBlock(keras.layers.Layer):
+    def __init__(self, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=keras.initializers.HeNormal, padding='same'):
+        super().__init__()
+
+        self.conv1 = keras.layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer, padding=padding)
+        self.conv2 = keras.layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer, padding=padding)
+        self.pool = keras.layers.MaxPool2D()
+    
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.conv2(x)
+        outputs = self.pool(x) # downsample
+
+        return outputs
+
+class ExpandingBlock(keras.layers.Layer):
+    def __init__(self, number_of_filters, kernel_size=3, activation='relu', kernel_initializer=initializers.HeNormal, padding='same'):
+        super().__init__()
+
+        self.conv1 = keras.layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer, padding=padding)
+        self.conv2 = keras.layers.Conv2D(number_of_filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer, padding=padding)
+        self.up = keras.layers.Conv2DTranspose(number_of_filters/2, 2, strides=2, activation=activation, kernel_initializer=kernel_initializer)
+        self.concat = keras.layers.Concatenate()
+
+    def call(self, inputs, contracting_counterpart):
+        x = self.conv1(inputs)
+        x = self.conv2(x)
+        x = self.up(x) # upsampling operation
+
+        # this allows for the cross connections from the contracting half of the unet
+        outputs = self.concat([tf.image.resize(contracting_counterpart, x.shape[1:3]), x])
+
+        return outputs
 
 class Unet(Model):
     def __init__(self, input_shape, K, starting_number_of_filters=64, levels=4, activation='relu', kernel_initializer=initializers.HeNormal, padding='same'):
+        super().__init__()
         self.K = K
         input = Input(shape = input_shape)
         
@@ -66,15 +102,28 @@ class Unet(Model):
 
 
 if __name__ == "__main__":
+    # this example is the bottom part of the original unet from the whitepaper
+    inputs = np.random.random((5,68,68,256))
 
-    # convolutions are valid, so output image is smaller than input image
-    model = Unet(input_shape=(572, 572, 1), K=3, padding='valid')
-    model.summary()
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy','val_accuracy'])
+    # test contracting block
+    con = ContractingBlock(512, padding='valid')
+    first_out = con(inputs)
+    print(first_out.shape) # should be (5, 32, 32, 512)
+
+    # test expanding block
+    counterpart = first_out
+    exp = ExpandingBlock(1024, padding='valid')
+    second_out = exp(first_out, counterpart)
+    print(second_out.shape) # should be (5, 56, 56, 1024)
+
+    # # convolutions are valid, so output image is smaller than input image
+    # model = Unet(input_shape=(572, 572, 1), K=3, padding='valid')
+    # model.summary()
+    # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy','val_accuracy'])
     
 
 
-    # convolutions are padded to produce same size output image
-    model2 = Unet(input_shape=(1024, 1024, 1), K=3)
-    model2.summary()
-    model2.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy','val_accuracy'])
+    # # convolutions are padded to produce same size output image
+    # model2 = Unet(input_shape=(1024, 1024, 1), K=3)
+    # model2.summary()
+    # model2.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy','val_accuracy'])
